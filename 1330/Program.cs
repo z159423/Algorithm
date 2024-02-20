7934009264cf2437d7942f39b0f72824220a084d
z159423 <tmdghks992@naver.com>
2024-02-21 03:09:00 +0900

    1330

---
+++ 1330/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+int A, B;
+
+string s = Console.ReadLine();
+string[] s1 = s.Split(' ');
+
+A = int.Parse(s1[0]);
+B = int.Parse(s1[1]);
+
+if(A > B)
+{
+    Console.WriteLine(">");
+}
+else if(A < B)
+{
+    Console.WriteLine("<");
+}
+else if(A == B)
+{
+    Console.WriteLine("==");
+}
