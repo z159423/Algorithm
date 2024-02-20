5e86cba79eed1979f6fa1940f9cd7f6c556308f4
z159423 <tmdghks992@naver.com>
2024-02-21 03:34:09 +0900

    2338

---
+++ 2338/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Numerics;
+
+BigInteger A = BigInteger.Parse(Console.ReadLine());
+BigInteger B = BigInteger.Parse(Console.ReadLine());
+
+Console.WriteLine(A + B);
+Console.WriteLine(A - B);
+Console.WriteLine(A * B);
