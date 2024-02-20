f30dc137015caefc6a22be4fbcf5daa2f3e739fa
z159423 <tmdghks992@naver.com>
2024-02-21 02:43:38 +0900

    1271

---
+++ 1271/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine(); // 사용자 입력 받기
+        string[] numbers = input.Split(' '); // 공백을 기준으로 문자열 분리
+
+        BigInteger money = BigInteger.Parse(numbers[0]);
+        BigInteger people = BigInteger.Parse(numbers[1]);
+
+        // BigInteger num1 = Console.Read();
+        // BigInteger num2 = Console.Read();
+
+        Console.WriteLine(money / people);
+        Console.WriteLine(money % people);
+    }
+}
